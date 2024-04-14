68adc318567229ce8d9d39a16e429a43d1651051
mgporter <26415062+mgporter@users.noreply.github.com>
2024-04-14 00:55:55 -0400

    added z-indexing and styling to terrain slider

--- src/gameui/TerrainSlider.tsx
+++ src/gameui/TerrainSlider.tsx
@@ -76,6 +76,8 @@ export default function TerrainSlider({
           max: mountainPercent - 1,
           min: 0,
         }
+        waterSliderRef.current.style.zIndex = "100";
+        mountainSliderRef.current.style.zIndex = "10";
         break; 
       }
 
@@ -86,6 +88,8 @@ export default function TerrainSlider({
           max: 100,
           min: waterPercent + 1,
         }
+        waterSliderRef.current.style.zIndex = "10";
+        mountainSliderRef.current.style.zIndex = "100";
         break;
       }
     }
@@ -105,16 +109,16 @@ export default function TerrainSlider({
         border-t-gray-500 border-l-gray-500 border-2 "
          style={{background: `linear-gradient(90deg, rgb(59,140,246) ${waterPercent}%, rgb(34, 197, 94) ${waterPercent+1}%, rgb(34, 197, 94) ${mountainPercent-1}%, rgb(167, 129, 89) ${mountainPercent}%)`}}>
 
-        <div 
-          onPointerDown={() => onPointerDown("water")} ref={waterSliderRef} 
-          className="slider box-content w-[8px] h-[20px] 
+        <div onPointerDown={() => onPointerDown("water")} ref={waterSliderRef} 
+          className="slider box-content w-[8px] h-[20px] group 
             absolute top-[-17px] left-[-14px] p-2 cursor-pointer">
 
           <div className="knob rounded-lg border-[1px] w-full h-full
-              border-gray-700 bg-slate-400 active:bg-slate-200">
+              border-gray-700 bg-slate-400 group-active:bg-slate-100 group-hover:bg-slate-300">
           </div>
 
-          <div className="label relative w-8 text-center bottom-[42px] left-[-12px]">
+          <div className="label relative w-8 text-center bottom-[40px] left-[-12px] text-xs
+            border-[1px] border-transparent group-active:border-black/60 rounded-lg group-active:bg-gray-600">
             {waterPercent}
           </div>
 
@@ -122,14 +126,15 @@ export default function TerrainSlider({
 
 
         <div onPointerDown={() => onPointerDown("mountain")} ref={mountainSliderRef} 
-          className="slider box-content w-[8px] h-[20px] 
+          className="slider box-content w-[8px] h-[20px] group 
           absolute top-[-17px] left-[-14px] p-2 cursor-pointer">
 
           <div className="knob rounded-lg border-[1px] w-full h-full
-              border-gray-700 bg-slate-400 active:bg-slate-200">
+              border-gray-700 bg-slate-400 group-active:bg-slate-200 group-hover:bg-slate-300">
           </div>
 
-          <div className="label relative w-8 text-center bottom-[42px] left-[-12px]">
+          <div className="label relative w-8 text-center bottom-[40px] left-[-12px] text-xs 
+            border-[1px] border-transparent group-active:border-black/60 rounded-lg group-active:bg-gray-600">
             {mountainPercent}
           </div>
 
